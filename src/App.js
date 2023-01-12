06731b95bfaf654b4db7165272c5344343fd08f7
Steven Duncan <Steven.Duncan@paccar.com>
2023-01-12 15:26:30 -0800

    end of section 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
-    return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
-    )
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {       
+        setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={deleteFeedback}
+        />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
