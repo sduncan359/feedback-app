f23e1ebaaa7e9af21fd86182cbd06cbafd3758c2
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-01-15 18:57:27 -0800

    end of section 6

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { v4 as uuidv4 } from "uuid";
 import AboutIconLink from "./components/AboutIconLink";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route 
-            exact 
-            path='/' 
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} /> 
-                <AboutIconLink />
-              </>
-            }>              
-          </Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                  <AboutIconLink />
+                </>
+              }>
+            </Route>
 
-          <Route path='/about' element={<AboutPage />}></Route>
-        </Routes>              
-      </div>
-    </Router>
+            <Route path='/about' element={<AboutPage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 import RatingSelect from "./RatingSelect";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
@@ -9,6 +9,16 @@ function FeedbackForm({ handleAdd }) {
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)      
+    }
+  }, [feedbackEdit])
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -33,7 +43,12 @@ function FeedbackForm({ handleAdd }) {
         rating,
       };
 
-      handleAdd(newFeedback)
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+       
+      } else {
+        addFeedback(newFeedback)
+      }
 
       setText('')
     }
@@ -59,10 +74,6 @@ function FeedbackForm({ handleAdd }) {
       </form>
     </Card>
   );
-}
-
-FeedbackForm.propTypes = {
-    handleAdd: PropTypes.func.isRequired
 }
 
 export default FeedbackForm;
